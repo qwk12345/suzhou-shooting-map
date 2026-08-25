4304e933e75d37dbe67d1cfde0e0c0883c6e0484
qwk12345 <136943849+qwk12345@users.noreply.github.com>
2026-08-25 23:03:18 +0800

    优化 iPhone 主屏幕应用体验

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "suzhou-shooting-map-v5";
+const CACHE_NAME = "suzhou-shooting-map-v6";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
