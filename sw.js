89e9a45c816140f83a4f007d59997624c03ff6af
qwk12345 <136943849+qwk12345@users.noreply.github.com>
2026-08-25 23:04:26 +0800

    完善 iPhone App 图标与安装配置

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "suzhou-shooting-map-v6";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "suzhou-shooting-map-v7";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./icon-180.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
