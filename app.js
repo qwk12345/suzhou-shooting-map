35d7b4973354c0a775cdc23d9dd09d9e9b79d3e4
qwk12345 <136943849+qwk12345@users.noreply.github.com>
2026-08-26 14:08:45 +0800

    强制刷新首页资源缓存

--- app.js
+++ app.js
@@ -388,4 +388,4 @@ renderBudget();
 renderLog();
 renderAnnual();
 
-if ("serviceWorker" in navigator) window.addEventListener("load", () => navigator.serviceWorker.register("sw.js"));
+if ("serviceWorker" in navigator) window.addEventListener("load", () => navigator.serviceWorker.register("sw.js?v=9"));
